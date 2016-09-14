b0ed2c8b4846d0ce015ca22c51575f64ce9ca190
wangzz <81303012@qq.com>
2016-09-14 17:10:25 +0800

    新增mapz类，此类事封装的一个Hashmap的工具类， 可以直接获取指定类型的变量

---
+++ utils/src/main/java/com/jngld/utils/Mapz.java
@@ -0,0 +1,147 @@
+package com.jngld.utils;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * Mapz
+ * @date：2016年9月14日11:56:28
+ */
+public class Mapz<K, V> extends HashMap<K, V> implements Serializable {
+
+    private Mapz() {
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    public Mapz set(K key, V value) {
+        this.put(key, value);
+        return this;
+    }
+
+    /**
+     * Get attribute of mysql type: varchar, char, enum, set, text, tinytext,
+     * mediumtext, longtext
+     */
+    public String getStr(String attr) {
+        return (String) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: int, integer, tinyint(n) n > 1, smallint,
+     * mediumint
+     */
+    public Integer getInt(String attr) {
+        return (Integer) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: bigint, unsign int
+     */
+    public Long getLong(String attr) {
+        return (Long) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: unsigned bigint
+     */
+    public java.math.BigInteger getBigInteger(String attr) {
+        return (java.math.BigInteger) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: date, year
+     */
+    public java.util.Date getDate(String attr) {
+        return (java.util.Date) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: time
+     */
+    public java.sql.Time getTime(String attr) {
+        return (java.sql.Time) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: timestamp, datetime
+     */
+    public java.sql.Timestamp getTimestamp(String attr) {
+        return (java.sql.Timestamp) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: real, double
+     */
+    public Double getDouble(String attr) {
+        return (Double) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: float
+     */
+    public Float getFloat(String attr) {
+        return (Float) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: bit, tinyint(1)
+     */
+    public Boolean getBoolean(String attr) {
+        return (Boolean) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: decimal, numeric
+     */
+    public java.math.BigDecimal getBigDecimal(String attr) {
+        return (java.math.BigDecimal) this.get(attr);
+    }
+
+    /**
+     * Get attribute of mysql type: binary, varbinary, tinyblob, blob,
+     * mediumblob, longblob
+     */
+    public byte[] getBytes(String attr) {
+        return (byte[]) this.get(attr);
+    }
+
+    /**
+     * Get attribute of any type that extends from Number
+     */
+    public Number getNumber(String attr) {
+        return (Number) this.get(attr);
+    }
+
+    /**
+     * Return attribute names of this model.
+     */
+    public String[] getAttrNames() {
+        Set<K> attrNameSet = this.keySet();
+        return attrNameSet.toArray(new String[attrNameSet.size()]);
+    }
+
+    /**
+     * Return attribute values of this model.
+     */
+    public Object[] getAttrValues() {
+        Collection<V> attrValueCollection = values();
+        return attrValueCollection.toArray(new Object[attrValueCollection
+                .size()]);
+    }
+
+    public static <K, V> Mapz<K, V> build() {
+        return new Mapz<K, V>();
+    }
+
+    public static void main(String[] args) {
+        /*Mapz<Object, Object> build = Mapz.build();
+        build.set("ddd", "ddd")
+        .set(1,1)
+        .set(2,2);
+        System.out.println(build.toString());*/
+    }
+
+}
